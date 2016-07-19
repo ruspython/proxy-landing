b79d2fbe901182ae7ebd2c2082123a9300d3d2ff
Andrey Medvedev <andr.medv.spb@gmail.com>
2016-07-19 12:40:53 +0300

    Revert "fixes #1774 #1786 #1795: Redirect to url with lang"
    This reverts commit 54fdd32292121a42e0acb13fd2a3c5cc7abce945.

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(function (req, res, next) {
         lang = isRussian ? 'ru' : 'en';
         res.cookie('lang', lang);
     } else {
+        req.url = '/' + lang + req.url;
         if (req.url.endsWith('/')) {
             res.cookie('lang', lang);
         }
@@ -39,10 +40,34 @@ app.use('/ru', proxy('0.0.0.0:3001', {
     }
 }));
 
-app.use('/', function(req, res) {
-    var langUrl = '/' + req.cookies.lang + req.url;
-    res.redirect(langUrl);
-});
+//app.use(express.static(__dirname + '/../soshace-landing-keystone/dist'));
+
+//app.use('/images', proxy('0.0.0.0:3001/images', {
+//    forwardPath: function(req, res) {
+//        return url.parse(req.url).path;
+//    }
+//}));
+//
+//app.use('/css', proxy('0.0.0.0:3001/css', {
+//    forwardPath: function(req, res) {
+//        return url.parse(req.url).path;
+//    }
+//}));
+//
+//app.use('/js', proxy('0.0.0.0:3001/js', {
+//    forwardPath: function(req, res) {
+//        return url.parse(req.url).path;
+//    }
+//}));
+
+//app.use('/', function (req, res, next) {
+//    var lang = acceptLanguage.get(req.headers["accept-language"]);
+//    if (lang.indexOf('ru') >= 0) {
+//        //console.log(arguments)
+//    } else {
+//
+//    }
+//});
 
 app.listen(7000, function () {
     console.log('Running app on port 7000');
